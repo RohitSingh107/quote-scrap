3173948d2b95bf7d92f15b6a98d47b9c61079592
Rohit <RohitSinghEmail@protonmail.com>
2023-08-05 18:43:06 +0530

    basic app

---
+++ app.py
@@ -0,0 +1,27 @@
+
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Function to scrape quotes
+def scrape_quotes():
+    url = "http://quotes.toscrape.com/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    quotes = []
+    for quote in soup.find_all("div", class_="quote"):
+        text = quote.find("span", class_="text").get_text()
+        author = quote.find("span", class_="author").get_text()
+        quotes.append({"text": text, "author": author})
+    return quotes
+
+# Route to display scraped quotes
+@app.route("/")
+def display_quotes():
+    quotes = scrape_quotes()
+    return render_template("index.html", quotes=quotes)
+
+if __name__ == "__main__":
+    app.run(debug=True)
